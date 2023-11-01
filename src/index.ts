d927965a03fcb4392516a2c195d34903ec1d10c1
prime-infinity <soe.a.prime@gmail.com>
2023-11-01 06:28:47 -0700

    primitives

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,9 @@ let age:number = 20
 if(age < 50){
     age += 10
 }
-console.log(age);
+//console.log(age);
+
+let sales:number = 123_456_789
+let course:string = "Typescript"
+let is_publised:boolean = true
+
