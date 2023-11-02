2c27c4b42a56790d3c42d29a90c51cb062587882
prime-infinity <soe.a.prime@gmail.com>
2023-11-01 18:11:15 -0700

    type-alias

--- src/typeAlisa.ts
+++ src/typeAlisa.ts
@@ -1,8 +1,12 @@
-let employee:{
+//using a type alias, we can define a custom type
+type Employee = {
     readonly id:number,
     name:string,
     retire:(data:Date) => void
-} = {id:1,
+}
+
+let employee2: Employee = {
+    id:1,
     name:"osamede",
     retire:(date:Date)=>{
         console.log(date);
