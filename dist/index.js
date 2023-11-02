e0e80f872fd20ba5edf0ca75448dfee2baffabdf
prime-infinity <soe.a.prime@gmail.com>
2023-11-01 18:48:44 -0700

    added gitignore

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,12 @@
 "use strict";
 let age = 20;
-if (age < 50)
+if (age < 50) {
     age += 10;
-console.log(age);
+}
+let sales = 123456789;
+let course = "Typescript";
+let is_publised = true;
+function render(document) {
+    console.log(document);
+}
 //# sourceMappingURL=index.js.map
